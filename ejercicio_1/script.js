6137c1b4fc914a33be8fcaa3685d97a104461dd8
julian2006 <hector_guillen@soy.sena.edu.co>
2023-09-19 21:57:13 -0500

    se agregaron ejercicios de javascript

---
+++ ejercicio_1/script.js
@@ -0,0 +1,139 @@
+//ejercicio 1
+
+
+/*let edad = prompt("ingrese su edad");
+
+
+edad = parseInt(edad);
+if (edad >= 18) {
+    alert("Eres mayor de edad");
+}
+else {
+    alert("Eres menor de edad");
+}*/
+
+
+//ejercicio 2
+
+/*let nota1 = parseFloat(prompt("Ingrese su calificación del 1 al 10"));
+let nota2 = parseFloat(prompt("Ingrese su calificación del 1 al 10"));
+let nota3 = parseFloat(prompt("Ingrese su calificación del 1 al 10"));
+
+let promedio = parseInt(nota1 + nota2 + nota3)/3;
+
+if (promedio > 6){
+    alert("Tu pormedio fue " +promedio.toFixed(2) +" aprobaste");
+}
+else{
+    alert("Tu pormedio fue " +promedio.toFixed(2) +" No probaste");
+}*/
+
+
+//ejercicio 3 
+
+/*let numeroA = parseFloat(prompt("ingrese un número del 1 al 100"));
+let numeroB = parseFloat(prompt("ingrese un número del 1 al 100"));
+
+if (numeroA < numeroB) {
+    alert(numeroA + " es menor que " + numeroB );
+}
+else if(numeroA <= numeroB){
+    alert(numeroA + " es igual que " + numeroB);
+}
+else {
+    alert(numeroA + " es mayor que " + numeroB);
+}*/
+
+
+//ejercico 4
+
+/*let numero1 = parseFloat(prompt("ingresa el primer número"));
+let numero2 = parseFloat(prompt("ingresa el segundo número"));
+
+if (numero1<numero2){
+    alert("en orden ascendente " + numero1 + "," + numero2);
+}
+else if(numero1>numero2){
+    alert("en orden ascendente " + numero2 + "," + numero1);
+}
+else{
+    alert("los numeros son iguales " + numero1);
+}*/
+
+
+//ejercicio 5
+
+/*let peso = prompt("ingrese su peso");
+
+
+peso = parseInt(peso)
+if(peso<=40){
+    alert("persona baja de peso")
+}
+else if(peso>71){
+    alert("persona con sobrepreso")
+}
+else{
+    alert("persona peso promedio")
+}*/
+
+
+//ejercicio 6
+
+/*let numero = prompt("escriba un número");
+ 
+numero = parseInt(numero)
+
+switch (numero) {
+
+    case 1: 
+    alert("Día "+numero+": lunes" );
+    break;
+    case 2: 
+    alert("Día "+numero+": martes" );
+    break;
+    case 3: 
+    alert("Día "+numero+": miercoles" );
+    break;
+    case 4: 
+    alert("Día "+numero+": jueves" );
+    break;
+    case 5: 
+    alert("Día "+numero+": viernes" );
+    break;
+    case 6: 
+    alert("Día "+numero+": sabado" );
+    break;
+    case 7: 
+    alert("Día "+numero+": domingo" );
+    break;
+    default:
+        alert("escribio un número fuera del rango, escriba un número entre 1 y 7")
+
+
+}*/
+
+
+//ejercicio 7
+
+/*let montocompra = prompt("ingrese el valor de su compra");
+
+montocompra = parseFloat(montocompra);
+
+let montofinal;
+
+if(montocompra > 1000){
+    montofinal = montocompra * 0.80;
+}
+else{
+    montofinal = montocompra * 0.95;
+}
+alert("el monto final a pagar es: $"+montofinal.toFixed(2))*/
+
+
+
+//ejercicio 8
+
+
+
+
